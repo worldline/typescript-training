6dada22ff32a95311854a11362a24c291a75ce85
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2025-07-22 10:19:13 +0200

    more alternate solutions for infer exercise

--- solutions/infer.ts
+++ solutions/infer.ts
@@ -11,6 +11,8 @@ type Pizza = [Topping, Topping, Topping];
 
 // Create a utility type ToppingOfPizza that receives a Pizza type and 
 // returns the union type representing one of the possible toppings of the pizza
+type ToppingOfPizza<P extends Pizza> = P[number];
+type ToppingOfPizza<P extends Pizza> = P[0] | P[1] | P[2];
 type ToppingOfPizza<P extends Pizza> = P extends [infer T1, infer T2, infer T3] ? T1 | T2 | T3 : never;
 
 type MyPizza = ['mushrooms', 'cheese', 'pepper'];
@@ -20,8 +22,8 @@ type MyToppings = ToppingOfPizza<MyPizza>; // expected to be 'mushrooms' | 'chee
 // Based on a list of toppings, return a type matching all the pizzas that can be made with these toppings
 type PossiblePizzas<Toppings extends Topping[]> = Toppings[number] extends infer T ? [T, T, T] : never;
 // other solutions:
-// type PossiblePizzas<Toppings extends Topping[]> = Toppings extends (infer T)[] ? [T, T, T] : never
-// type PossiblePizzas<Toppings extends Topping[]> = [Toppings[number],Toppings[number],Toppings[number]]
+type PossiblePizzas<Toppings extends Topping[]> = Toppings extends (infer T)[] ? [T, T, T] : never
+type PossiblePizzas<Toppings extends Topping[]> = [Toppings[number],Toppings[number],Toppings[number]]
 
 type InStock = PossiblePizzas<["bacon", "cheese", "olives", "onions"]>
 
@@ -29,9 +31,13 @@ const pizza1: InStock = ["onions", "cheese", "olives"]
 const pizza2: InStock = ["cheese", "olives", "bacon"]
 const pizza3: InStock = ["bacon", "cheese", "mushrooms"] // <-- should not be valid
 
-// The Pizza store has an infinite supply of cheese onions and olives
+// The Pizza store has an infinite supply of cheese and olives
 // Make an AvailablePizzas that use these 3 toppings as default value for the Toppings parameter
-type AvailablePizzas<Toppings extends Topping[] = ["cheese", "onions", "olives"]> = Toppings[number] extends infer T ? [T, T, T] : never;
+type InfiniteSupplies = ["cheese", "olives"];
+type InfiniteSupply = InfiniteSupplies[number];
+type AvailablePizzas<Toppings extends Topping[] = InfiniteSupplies> = Toppings[number] | InfiniteSupply extends infer T 
+? [T,T,T] 
+: never;
 
 const pizza4: AvailablePizzas = ["cheese", "cheese", "cheese"]
 const pizza5: AvailablePizzas<["cheese", "mushrooms", "bacon"]> = ["bacon", "mushrooms", "cheese"]
